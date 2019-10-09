180fd71d12fc60f019fac9e3a2e154dda3031676
Aaron Linville <aaron@linville.org>
2019-10-08 22:22:12 -0400

    Let user specify file to create when exporting an XLSX.
    Minor readme updates.

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 cmmcdb
 ======
 
-Django ActiveRecord structure to to store the [Cybersecurity Model Maturity Certification model framework](https://www.acq.osd.mil/cmmc/). Includes various Python utilities for converting the PDF distributed by
-OUSD(A&S) into a database and for exporting the database as various other formats.
+CMMCDB is a suite of utilities for importing the [Cybersecurity Model Maturity Certification model framework](https://www.acq.osd.mil/cmmc/) into a normalized database structured (i.e.: Django active record model). It includes capabilities for importing the original PDF distributed by
+OUSD(A&S) and then exporting various other formats including OpenXML and JSON.
 
 
 Requirements
-============
-* Python 3
-* Django, for basic usage and custom format exporting.
-* camelot-py, for original conversion of PDF into normalized database structure.
+------------
+* [Django](https://www.djangoproject.com), Python app framework used for active record, database abstraction, etc.
+* [camelot-py](https://camelot-py.readthedocs.io/en/master/), for parsing the PDF and extraction of tabular data.
 
 
 Setup Environment
-=================
+-----------------
     # Get the code
     git clone https://github.com/linville/cmmcdb.git
     cd cmmcdb
@@ -25,7 +24,7 @@ Setup Environment
 
 
 Basic Usage
-===========
+-----------
     # Create the sqlite database
     ./manage.py migrate
     
@@ -37,3 +36,6 @@ Basic Usage
     
     # Export to an Microsoft Excel compatible OpenXML document
     ./manage.py exportxlsx
+    
+    # Export to JSON (formatted as a Django fixture)
+    ./manage.py dumpdata --indent 2 cmmcdb
--- cmmcdb/management/commands/exportxlsx.py
+++ cmmcdb/management/commands/exportxlsx.py
@@ -19,27 +19,30 @@ class Command(BaseCommand):
 
     help = "Exports the CMMC database structure as Open XML spreadsheet."
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "FILE",
+            nargs="?",
+            type=argparse.FileType("wb"),
+            default="cmmc.xlsx",
+            help="Path to the CMMC spreadsheet to create.",
+        )
+
     def handle(self, *args, **options):
         self.wb = Workbook()
         self.define_styles()
         self.wb_row = 1
         # wb.active
-        
+
         self.wb.freeze_panes = "A1"
-        c = self.wb.active.cell(
-            column=1,
-            row=self.wb_row,
-            value=f"Capability",
-        )
+        c = self.wb.active.cell(column=1, row=self.wb_row, value=f"Capability")
         c.style = "Maturity Level"
-        
+
         self.ml = {}
         for ml in MaturityLevel.objects.order_by("level").all():
             self.ml[ml.level] = ml
             c = self.wb.active.cell(
-                column=ml.level + 1,
-                row=self.wb_row,
-                value=f"Level {ml.level}",
+                column=ml.level + 1, row=self.wb_row, value=f"Level {ml.level}"
             )
             c.style = "Maturity Level"
         self.wb_row += 1
@@ -78,7 +81,7 @@ class Command(BaseCommand):
                 self.export_capability(capability)
             self.wb_row += 1
 
-        self.wb.save(filename=("cmmc.xlsx"))
+        self.wb.save(filename=options["FILE"].name)
 
     def export_capability(self, capability):
         max_index = capability.activities.aggregate(Max("index"))["index__max"]
@@ -114,34 +117,28 @@ class Command(BaseCommand):
         s.font = Font(size=16, bold=True)
         s.fill = PatternFill("solid", fgColor="fff1ce")
         self.wb.add_named_style(s)
-        
+
         # Maturity Level Styles
         s = NamedStyle(name="Maturity Level")
         s.font = Font(size=12, bold=True, color="ffffff")
         s.fill = PatternFill("solid", fgColor="0000ff")
         s.alignment = Alignment(horizontal="center")
         self.wb.add_named_style(s)
-        
+
         # Practice
         s = NamedStyle(name="Practice")
         s.font = Font(size=12, bold=True)
-        s.alignment = Alignment(
-            horizontal="general", vertical="top", wrap_text=True
-        )
+        s.alignment = Alignment(horizontal="general", vertical="top", wrap_text=True)
         self.wb.add_named_style(s)
-        
+
         # Process
         s = NamedStyle(name="Process")
         s.font = Font(size=12, bold=True)
         s.fill = PatternFill("solid", fgColor="ddddff")
-        s.alignment = Alignment(
-            horizontal="general", vertical="top", wrap_text=True
-        )
+        s.alignment = Alignment(horizontal="general", vertical="top", wrap_text=True)
         self.wb.add_named_style(s)
-        
+
         # Activity
         s = NamedStyle(name="Activity")
-        s.alignment = Alignment(
-            horizontal="general", vertical="top", wrap_text=True
-        )
+        s.alignment = Alignment(horizontal="general", vertical="top", wrap_text=True)
         self.wb.add_named_style(s)
